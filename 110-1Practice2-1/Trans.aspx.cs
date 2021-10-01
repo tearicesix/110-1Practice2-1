171285a7e38cb67d5a1b1acfccc70bb955e5c2ad
偉翔 黃 <108111109@mail.aeust.edu.tw>
2021-10-01 12:05:38 +0800

    Trans

---
+++ 110-1Practice2-1/Trans.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_1
+{
+    public partial class Trans : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double d_Value, c;
+            d_Value = 66;
+            c = (d_Value - 32) / (9 / 5);
+            Response.Write("d_Value = " + d_Value + "F = " + c + "C");
+        }
+    }
+}
