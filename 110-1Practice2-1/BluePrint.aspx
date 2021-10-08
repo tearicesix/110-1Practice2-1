adfebde468417c6603465b9651b07af6e85b2874
偉翔 黃 <108111109@mail.aeust.edu.tw>
2021-10-08 10:27:37 +0800

    BluePrint

---
+++ 110-1Practice2-1/BluePrint.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BluePrint.aspx.cs" Inherits="_110_1Practice2_1.BluePrint" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 110-1Practice2-1/BluePrint.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_1
+{
+    public partial class BluePrint : System.Web.UI.Page
+    {
+        public class Man {
+            public string s_Id;
+            public float f_Hei;
+            public float f_Wei;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Man o_Man = new Man();
+            o_Man.s_Id = "A123456789";
+            o_Man.f_Hei = 178.5f;
+            o_Man.f_Wei = 72.8f;
+
+            //s_Id= 值, f_Hei= 值, f_Wei= 值
+            Response.Write("s_Id=" + o_Man.s_Id + ", " +
+                           "f_Hei=" + o_Man.f_Hei.ToString() + ", " +
+                           "f_Wei =" + o_Man.f_Wei.ToString()
+                           );
+        }
+    }
+}
